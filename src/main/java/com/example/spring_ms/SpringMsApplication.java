1948c446ed27bfdd6fec39dad5cd2ac0df010ae2
2B813 <2B813@CP162W202B813.cfe.mx>
2025-05-19 18:18:09 -0600

    prueba commit

--- src/main/java/com/example/spring_ms/SpringMsApplication.java
+++ src/main/java/com/example/spring_ms/SpringMsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMsApplication {
 
+//El poderoso
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMsApplication.class, args);
 	}
